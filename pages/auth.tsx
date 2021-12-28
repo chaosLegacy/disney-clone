3f6aab7d43adeeb1782522789812d4a4521fb2c2
Youssef Barnoukh <youssef.barnoukh@gmail.com>
2021-12-28 14:36:16 +0100

    Add screens: auth, home, details/:id auth design done

---
+++ pages/auth.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from '../styles/Auth.module.css';
+
+const Auth = () => {
+    return (
+        <>
+            <section className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.wrap}>
+                        <img src="/images/cta-logo-one.svg" alt="logo" className={styles.logo} />
+                        <a href="#" className={styles.signUp}>Get All there</a>
+                        <p className={styles.description}>
+                            Get Premier Access to Raya and the Last Dragon for an additional fee
+                            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                            and The Disney Bundle will increase by $1.
+                        </p>
+                        <img src="/images/cta-logo-two.png" alt="logo two" className={styles.logo} />
+                    </div>
+                    <div className={styles.background} />
+                </div>
+            </section>
+        </>
+    )
+}
+export default Auth
